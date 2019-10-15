e59263db4581079b274239f5e73d35b222d0ef5d
william <william.rehel@gmail.com>
2019-10-15 12:19:53 -0300

    Comienzo actividad final DESSO

---
+++ actividadfinal/src/actividadfinal/Doctor.java
@@ -0,0 +1,51 @@
+package actividadfinal;
+
+public class Doctor extends Persona {
+    String especialidad;
+    int anhiosExperiencia;
+    int sueldoBase;
+
+    public Doctor() {
+        super();
+        this.especialidad = "";
+        this.anhiosExperiencia = 0;
+        this.sueldoBase = 0;
+    }
+
+    public Doctor(String especialidad, int anhiosExperiencia, int sueldoBase, String rut, String pnombre, String snombre, String apaterno, String amaterno, String edad, char sexo) {
+        super(rut, pnombre, snombre, apaterno, amaterno, edad, sexo);
+        this.especialidad = especialidad;
+        this.anhiosExperiencia = anhiosExperiencia;
+        this.sueldoBase = sueldoBase;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getAnhiosExperiencia() {
+        return anhiosExperiencia;
+    }
+
+    public void setAnhiosExperiencia(int anhiosExperiencia) {
+        this.anhiosExperiencia = anhiosExperiencia;
+    }
+
+    public int getSueldoBase() {
+        return sueldoBase;
+    }
+
+    public void setSueldoBase(int sueldoBase) {
+        this.sueldoBase = sueldoBase;
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" + "especialidad=" + especialidad + ", anhiosExperiencia=" + anhiosExperiencia + ", sueldoBase=" + sueldoBase + '}';
+    }
+       
+}
---
+++ actividadfinal/src/actividadfinal/Persona.java
@@ -0,0 +1,93 @@
+package actividadfinal;
+
+public abstract class Persona {
+    private String rut;
+    private String pnombre;
+    private String snombre;
+    private String apaterno;
+    private String amaterno;
+    private String edad;
+    private char sexo;
+
+    public Persona() {
+        this.rut = "";
+        this.pnombre = "";
+        this.snombre = "";
+        this.apaterno = "";
+        this.amaterno = "";
+        this.edad = "";
+        this.sexo = ' ';
+    }
+
+    public Persona(String rut, String pnombre, String snombre, String apaterno, String amaterno, String edad, char sexo) {
+        this.rut = rut;
+        this.pnombre = pnombre;
+        this.snombre = snombre;
+        this.apaterno = apaterno;
+        this.amaterno = amaterno;
+        this.edad = edad;
+        this.sexo = sexo;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String getPnombre() {
+        return pnombre;
+    }
+
+    public void setPnombre(String pnombre) {
+        this.pnombre = pnombre;
+    }
+
+    public String getSnombre() {
+        return snombre;
+    }
+
+    public void setSnombre(String snombre) {
+        this.snombre = snombre;
+    }
+
+    public String getApaterno() {
+        return apaterno;
+    }
+
+    public void setApaterno(String apaterno) {
+        this.apaterno = apaterno;
+    }
+
+    public String getAmaterno() {
+        return amaterno;
+    }
+
+    public void setAmaterno(String amaterno) {
+        this.amaterno = amaterno;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "rut=" + rut + ", pnombre=" + pnombre + ", snombre=" + snombre + ", apaterno=" + apaterno + ", amaterno=" + amaterno + ", edad=" + edad + ", sexo=" + sexo + '}';
+    }
+    
+}
